2d5a48be12f7816355f810c0836f79bf2594a2fb
J-sckep <juameza58@gmail.com>
2021-10-25 18:58:15 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Burbuja = require('./metodoburbuja');
 const Insercion = require('./Insercion');
 
+console.log('Metodo Burbuja :');
 Burbuja();
 
+console.log('metodo de Insercion :');
 Insercion();
